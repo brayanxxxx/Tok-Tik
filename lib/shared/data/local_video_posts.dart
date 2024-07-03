c9924ab9ffb677ffb23c9eb2a706866aaccf506d
brayan <brayancordoba865@gmail.com>
2024-07-03 17:17:13 -0500

    Tok Tik

---
+++ lib/shared/data/local_video_posts.dart
@@ -0,0 +1,54 @@
+List<Map<String, dynamic>> videoPosts = [
+  {
+    'name': 'Subiendo escaleras automáticas',
+    'videoUrl': '',
+    'likes': 23230,
+    'views': 1523,
+  },
+  {
+    'name': 'Planta apreciada por peatones',
+    'videoUrl': '',
+    'likes': 24230,
+    'views': 1343,
+  },
+  {
+    'name': 'Que borroso veo todo!',
+    'videoUrl': '',
+    'likes': 21564320,
+    'views': 123563,
+  },
+  {
+    'name': '¿Esto es trigo? que interesante',
+    'videoUrl': '',
+    'likes': 320,
+    'views': 2300,
+  },
+  {
+    'name': 'El COVID no me afecta',
+    'videoUrl': '',
+    'likes': 3230,
+    'views': 31030,
+  },
+  {
+    'name': 'No quiero ir a trabajar hoy señor Stark',
+    'videoUrl': '',
+    'likes': 10,
+    'views': 330,
+  },
+  {
+    'name': 'Limpiar nunca fue tan divertido',
+    'videoUrl': '',
+    'likes': 1320,
+    'views': 33032,
+  },
+  {
+    'name': '¿Ya llegamos a la India?... umm si',
+    'videoUrl': '',
+    'likes': 342,
+    'views': 3332,
+  },
+
+
+];
+
+
